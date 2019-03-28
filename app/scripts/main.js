18e79a190c842b6927b20908e4615695e4f7133c
abdulrahim.ka@luminescent-digital.com <abdulrahim.ka@luminescent-digital.com>
2019-03-28 11:52:41 +0530

    Registered Service worker

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,7 +21,7 @@
 
 'use strict';
 
-const applicationServerPublicKey = '<Your Public Key>';
+const applicationServerPublicKey = 'BMkgBTTBLHC88LN1Hqo2fXYowf2DQpioAYI_FW1GAD6ivQmXtDDzLB-gNMu3IrR56UAlcQzuAVwcZ49P8NcNQlM';
 
 const pushButton = document.querySelector('.js-push-btn');
 
@@ -41,4 +41,25 @@ function urlB64ToUint8Array(base64String) {
     outputArray[i] = rawData.charCodeAt(i);
   }
   return outputArray;
+}
+
+
+/********Register Service worker********** */
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+
+  navigator.serviceWorker.register('sw.js')
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
+
+    swRegistration = swReg;
+
+
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
+  });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
 }
